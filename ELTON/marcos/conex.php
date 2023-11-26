cecbb8c4f99e1c280e439192836f75ada35586f3
strongmen1234 <152052864+strongmen1234@users.noreply.github.com>
2023-11-26 00:37:41 -0600

    hola

--- ELTON/marcos/conex.php
+++
@@ -1,8 +0,0 @@
-<?php
- $servername="localhost";
- $username="root";
- $password="";
- $database="Proyecto";
-
- $conexion = mysqli_connect($servername, $username, $password, $database) or die ("Conexion fallida");
-?>
